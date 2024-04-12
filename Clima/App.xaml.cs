1a45e52aab214eef1e256210177e9c2315fc568e
ManuMaria <emanuelebrumrossi0@gmail.com>
2024-04-12 07:56:11 -0300

    início do jogo

---
+++ Clima/App.xaml.cs
@@ -0,0 +1,11 @@
+﻿namespace Clima;
+
+public partial class App : Application
+{
+	public App()
+	{
+		InitializeComponent();
+
+		MainPage = new AppShell();
+	}
+}
---
+++ Clima/AppShell.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace Clima;
+
+public partial class AppShell : Shell
+{
+	public AppShell()
+	{
+		InitializeComponent();
+	}
+}
